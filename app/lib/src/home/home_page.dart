05ac3c159cb51abc710f2d3cfdcc0a23575e3ca0
Gabriel <gabrielblandy@gmail.com>
2023-02-20 18:29:45 -0300

    Aula 10

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:listinha/routes.dart';
 import 'package:listinha/src/home/widgets/cursom_drawer.dart';
+import 'package:listinha/src/home/widgets/task_card.dart';
+import 'package:listinha/src/shared/services/realm/models/task_model.dart';
 import 'package:listinha/src/shared/widgets/user_image_button.dart';
+import 'package:realm/realm.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,31 +35,62 @@ class _HomePageState extends State<HomePage> {
       ),
       drawer: const CustomDrawer(),
       body: Center(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: SegmentedButton<int>(
-                segments: const [
-                  ButtonSegment(
-                    value: 0,
-                    label: Text('Todos'),
-                  ),
-                  ButtonSegment(
-                    value: 1,
-                    label: Text('Pendentes'),
-                  ),
-                  ButtonSegment(
-                    value: 2,
-                    label: Text('Concluídas'),
-                  ),
-                  ButtonSegment(
-                    value: 3,
-                    label: Text('Desativadas'),
-                  ),
-                ],
-                selected: const {1},
-                onSelectionChanged: (values) {},
+            ListView.separated(
+              padding: const EdgeInsets.only(
+                top: 65,
+                left: 20,
+                right: 20,
+                bottom: 90,
+              ),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                final board = TaskBoard(
+                  Uuid.v4(),
+                  'Nova Lista de tarefas 1',
+                  tasks: [
+                    Task(Uuid.v4(), '', complete: true),
+                    Task(Uuid.v4(), '', complete: true),
+                    Task(Uuid.v4(), '', complete: true),
+                    Task(Uuid.v4(), '', complete: true),
+                  ],
+                );
+                return TaskCard(
+                  board: board,
+                  height: 140,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 10);
+              },
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SegmentedButton<int>(
+                  segments: const [
+                    ButtonSegment(
+                      value: 0,
+                      label: Text('Todos'),
+                    ),
+                    ButtonSegment(
+                      value: 1,
+                      label: Text('Pendentes'),
+                    ),
+                    ButtonSegment(
+                      value: 2,
+                      label: Text('Concluídas'),
+                    ),
+                    ButtonSegment(
+                      value: 3,
+                      label: Text('Desativadas'),
+                    ),
+                  ],
+                  selected: const {1},
+                  onSelectionChanged: (values) {},
+                ),
               ),
             ),
           ],
